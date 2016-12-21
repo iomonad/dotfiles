1f4b34e3efe116a6dc469fd4cf235eb09f1d0954
iomonad <seyt@protonmail.ch>
2016-12-21 21:40:37 +0100

    Updated ZSH paths

--- zsh/02-exports.zsh
+++ zsh/02-exports.zsh
@@ -2,6 +2,37 @@
 
 # Some Classic {{{
 export HOSTNAME="darknet"
+export USER_NAME="iomonad"
+export USER_MAIL="iomonad@riseup.net"
+export USER_CITY="Malmö, SE"
+export USER_WEBSITE="https://inother.space/"
+export USER_FAVORITE_EDITOR="vim"
+export USER_FAVORITE_IDE="emacs"
+export USER_FAVORITE_BROWSER_GUI="qutebrowser"
+export USER_FAVORITE_BROWSER_CLI="w3m"
+export USER_FAVORITE_PAGER="less"
+export USER_FAVORITE_MEDIA_PLAYER="mplayer"
+export USER_FAVORITE_MUSIC_PLAYER="ncmpcpp"
+export USER_FAVORITE_MAIL_READER="mutt"
+export USER_FAVORITE_LANGUAGE="haskell"
+export USER_FAVORITE_SCRIPT_LANGUAGE="perl"
+export USER_FAVORITE_SHELL="$SYSTEM_BOOTSTAGE_BIN/zsh"
+export USER_FAVORITE_OFFICE_APP="libreoffice"
+export USER_FAVORITE_KEYMAP="dvorak"
+export USER_FAVORITE_PDF_READER="zathura"
+export USER_FAVORITE_IMAGE_VIEWER="sxiv"
+export USER_FAVORITE_IRC_CLIENT="weechat"
+export USER_FAVORITE_MUSIC_STYLE="ACID"
+export USER_FAVORITE_OS="gentoo"
+export USER_FAVORITE_WM="xmonad"
+export USER_FAVORITE_BUILD_TOOL="make"
+export USER_FAVORITE_JAVA_BUILD_TOOL="gradle"
+export USER_FAVORITE_HASKELL_BUILD_TOOL="stack"
+export USER_PHONE_OS="cyanogenmod"
+export USER_FAVORITE_SEARCH_ENGINE="https://duckduckgo.com"
+export USER_FAVORITE_PLAYLIST="https://www.youtube.com/playlist?list=PLB_qb4K3oqyEA7VHJtxs7ehAL8cHVwCnb"
+export USER_FAVORITE_PHILOSOPH="Noam Chomsky"
+export USER_FAVORITE_QUOTE="The more you can increase fear of drugs and crime, welfare mothers, immigrants and aliens, the more you control all the people."
 export ZSH_THEME="g0tr00t"
 export ZSH_ACTIVE_COMPLETIONS="$( echo ${(kv)_comps[@]} )"
 export EDITOR=vim
@@ -41,6 +72,11 @@ export VI_QUICKFIX_SOURCEFILE='stdin'
 # My HardWare {{{
 export LP_DEST="Canon_MP150"
 export PRINTER="Canon_MP150"
+export PHONE="Huawei PLK-l01"
+export SSD="toshiba"
+export GFX_CARD="Nvidia GTK 750"
+export ETHERNET_CONTROLER="Qualcomm Atheros Killer E220x"
+export IOT_CARD="Beagle_Bone_Black"
 #}}}
 # Zsh History Hack {{{
 export HISTFILE=$XDG_DATA_HOME/zsh/history
@@ -51,14 +87,15 @@ export HISTIGNORE="&:ls:[bf]g:exit:reset:clear:cd*"
 # Some Pango Fix {{{
 export MOZ_DISABLE_PANGO=1
 #export CLIVE_CONFIG="/home/scp1/etc/cliverc"
+
 # The Path {{{
 export GEM_PATH='/home/seyt/.gem/ruby/2.2.0/bin/'
-export PATH=$ANDROID_HOME/tools/:$ANDROID_HOMEbuild-tools/25.0.1/:$ANDROID_HOME/platform-tools/:$GEM_PATH:~/.cabal/bin:/opt/jdk/bin:~/bin:/bin:/usr/bin:/usr/local/bin:/usr/bin/site_perl:/usr/bin/core_perl:/usr/bin/vendor_perl:/usr/lib/perl5/site_perl/bin:/usr/lib/perl5/vendor_perl/bin:/usr/lib/perl5/core_perl/bin:/sbin:/usr/sbin:/usr/bin/perlbin/site:/usr/bin/perlbin/vendor:/usr/bin/perlbin/core:/home/scp1/perl5/perlbrew/perls/perl-5.8.3/:$HOME/go/bin
+export PATH=$GEM_PATH:~/.cabal/bin:/opt/jdk/bin:~/bin:/bin:/usr/bin:/usr/local/bin:/usr/bin/site_perl:/usr/bin/core_perl:/usr/bin/vendor_perl:/usr/lib/perl5/site_perl/bin:/usr/lib/perl5/vendor_perl/bin:/usr/lib/perl5/core_perl/bin:/sbin:/usr/sbin:/usr/bin/perlbin/site:/usr/bin/perlbin/vendor:/usr/bin/perlbin/core:/home/scp1/perl5/perlbrew/perls/perl-5.8.3/:$HOME/go/bin
 export MANPATH=$MANPATH:/usr/local/man:/opt/local/share/man
 #}}}
 
 # Android sdk
-export ANDROID_TOOLS="~/android/sdk/build-tools/24.0.2/:~/android/sdk/platform-tools/:~/android/sdk/tools/"
+export ANDROID_TOOLS=
 export MONGODB_PATH="mongodb://monabot:secretpass@localhost:27017/irc-bot-db"
 # Fonts For System, Useless {{{
 export FONT='-windows-montecarlo-medium-r-normal--0-0-72-72-c-0-microsoft-cp1252'
@@ -104,5 +141,48 @@ export LISTMAX=300
 #export PERL_UNICODE=1
 #export PERL_RL=Zoid
 #}}}
-export ANDROID_HOME="$HOME/Android/Sdk/build-tools"
+
+# Ruby Path
+export GEM_PATH='/home/seyt/.gem/ruby/2.2.0/bin/'
+
+# Android PATH {{{
+export ANDROID_HOME="$HOME/android/sdk/"
+export ANDROID_SDK_VER="25.0.1"
+export ANDROID_BUILD_TOOLS="$ANDROID_HOME/build-tools/$ANDROID_SDK_VER/"
+export ANDROID_PLATFORM_TOOLS="$ANDROID_HOME/platform-tools/"
+export ANDROID_PATHS="$ANDROID_BUILD_TOOLS:$ANDROID_PLATFORM_TOOLS"
+# }}}
+
+# Java path {{{
+export JAVA_BIN_PATH="/opt/jdk/bin/"
+# }}}
+
+# Haskell related {{{
+export CABAL_BIN_PATH="$HOME/.cabal/bin"
+#export STACK_BIN_PATH="$HOME/.stack/bin" # Nope
+# }}}
+
+# User binary path {{{
+export USER_BIN_PATH="$HOME/bin"
+export SYSTEM_BOOTSTAGE_BIN="/bin"
+export SYSTEM_SU_BIN="/sbin"
+export SYSTEM_WIDE_BIN="/usr/bin"
+export SYSTEM_SU_WIDE_BIN="/usr/sbin"
+export SYSTEM_HOST_BIN="/usr/local/bin"
+export SYSTEM_PATHS="$USER_BIN_PATH:$SYSTEM_BOOTSTAGE_BIN:$SYSTEM_SU_BIN:$SYSTEM_WIDE_BIN:$SYSTEM_SU_WIDE_BIN:$SYSTEM_HOST_BIN" # Centralized
+# }}}
+# Perl Paths {{{
+export PERL_SITE_BIN="$SYSTEM_WIDE_BIN/site_perl"
+export PERL_CORE_PATH="$SYSTEM_WIDE_BIN/core_perl"
+export PERL_VENDOR_PATH="$SYSTEM_WIDE_BIN/vendor"
+export PERL5_SITE_BIN="/usr/lib/site_perl/bin"
+export PERL5_CORE_PATH="/usr/lib/perl5/core_perl/bin"
+export PERL5_VENDOR_PATH="/usr/lib/perl5/vendor_perl/bin"
+export PERL_PATHS="$PERL_SITE_BIN:$PERL_CORE_PATH:$PERL_VENDOR_PATH:$PERL5_SITE_BIN:$PERL5_CORE_PATH:$PERL5_VENDOR_PATH"
+# }}}
+
+# The Glorious Path {{{
+export PATH="$SYSTEM_PATHS:$GEM_PATH:$CABAL_PATH:$JAVA_BIN_PATH:$PERL_PATHS:$ANDROID_PATHS"
+#}}}
+
 # vim: set ts=2 expandtab sw=2:
