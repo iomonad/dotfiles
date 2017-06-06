ff678cbebd30557540c4c2698ce710277f5d6fe5
La Monade <iomonad@riseup.net>
2017-06-07 00:00:56 +0200

    Updated zsh aliases.

--- zsh/02-alias.zsh
+++ zsh/02-alias.zsh
@@ -274,6 +274,22 @@ alias esearchdesc="sudo emerge --searchdesc"
 alias newsall="sudo eselect news read all"
 alias newslist="sudo eselect news list"
 alias newspurge="sudo eselect news purge"
+
+alias eqf='equery f'
+alias equ='equery u'
+alias eqh='equery h'
+alias eqa='equery a'
+alias eqb='equery b'
+alias eql='equery l'
+alias eqd='equery d'
+alias eqg='equery g'
+alias eqc='equery c'
+alias eqk='equery k'
+alias eqm='equery m'
+alias eqy='equery y'
+alias eqs='equery s'
+alias eqw='equery w'
+
 # Should be executed when new kernel update
 alias nvidia-rebuild-drivers="emerge @module-rebuild"
 #}}}
