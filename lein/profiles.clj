ff3b73c4335a46acc4516496635f994ccdb544ec
Clement Trosa <me@trosa.io>
2018-02-08 19:09:04 +0100

    WHO THE FUCK CAME UP WITH MAKE?

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,2 @@
-{:user {:plugins [[venantius/ultra "0.5.0"]
-                  [lein-bin "0.3.5"]                  
-                  [lein-try "0.4.3"]
-                  [lein-exec "0.3.6"]
-                  [lein-ancient "0.6.10"]
-                  [lein-bikeshed "0.4.1"]
-                  [lein-cloverage "1.0.9"]
-                  [lein-topology "0.2.0"]                  
-                  [lein-hiera "0.9.5"]
-                  [walmartlabs/vizdeps "0.1.6"]]}}
+{:user {:plugins [[venantius/ultra "0.5.2"]
+                  [lein-cloverage "1.0.9"]]}}
