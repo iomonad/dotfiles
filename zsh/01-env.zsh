9cec2acfa5d836d8e823e6877f583855c5117115
Clement Trosa <me@trosa.io>
2018-04-10 22:10:47 +0200

    #GrammarNazi

--- zsh/01-env.zsh
+++ zsh/01-env.zsh
@@ -10,9 +10,10 @@ export PAGER='less'
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
+export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 
 # paths
-export PATH=$PATH:$HOME/documents/bins
+export PATH=$PATH:$HOME/documents/bins:$HOME/.local/bin
 
 PATH="$HOME/devels/perl5/bin${PATH:+:${PATH}}"; export PATH;
 PERL5LIB="$HOME/devels/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
