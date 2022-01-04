60348d19d9a0571e304d229f93ac4989eadf191b
iomonad <iomonad@riseup.net>
2022-01-05 00:00:19 +0100

    kernel config backup

--- gentoo/sync.sh
+++ gentoo/sync.sh
@@ -2,3 +2,4 @@ rsync -rvaup /etc/portage/make.conf .
 rsync -rvaup /etc/portage/repos.conf .
 rsync -rvaup /etc/portage/package.use .
 rsync -rvaup /var/lib/portage/world .
+rsync -rvaup /boot/config* kernel
