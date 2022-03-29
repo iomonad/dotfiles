44429fa644a9f5a5619599258e5238ae6fe40a65
citrouille <clement.trouillard@oscaro.com>
2022-03-29 14:19:08 +0200

    mise a jour devkit

---
+++ leiningen/profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [[jonase/eastwood "0.9.9"]
+                  [lein-ancient "1.0.0-RC3"]
+                  [lein-marginalia "0.9.1"]
+                  [io.aviso/pretty "1.1.1"]]
+        :dependencies [[io.aviso/pretty "1.1.1"]]
+        :middleware [io.aviso.lein-pretty/inject]}}
--- zsh/05-devkits.zsh
+++ zsh/05-devkits.zsh
@@ -29,6 +29,12 @@ export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 #
+# Cargo
+#
+
+export PATH="$PATH:$HOME/.cargo/bin"
+
+#
 # Google SDK
 #
 
