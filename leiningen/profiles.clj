f2f578bcbb8c140a70abe89a4b2d3e61b5c0f57b
iomonad <iomonad@riseup.net>
2023-07-30 11:20:19 +0200

    lein: updated profile

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -4,6 +4,7 @@
                   [io.aviso/pretty "1.1.1"]
                   [lein-cljfmt "0.8.0"]
 		  [lein-cloverage "1.2.2"]
+                  [venantius/yagni "0.1.7"]
                   [lein-marginalia "0.9.1"]]
         :dependencies [[io.aviso/pretty "1.1.1"]]
         :middleware [io.aviso.lein-pretty/inject]}}
