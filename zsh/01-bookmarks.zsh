80ee6b70749dcf98b38c5dc068791cd3cb6b1bb8
iomonad <iomonad@riseup.net>
2024-02-10 11:52:53 +0100

    zsh: bookmarks updated

--- zsh/01-bookmarks.zsh
+++ zsh/01-bookmarks.zsh
@@ -1,6 +1,10 @@
 # 02-bookmarks.zsh
 # (c) 2015 iomonad <iomonad@riseup.net>
 
-hash -d w=~/Work/
+hash -d w=~/code/
 hash -d z=~/.zsh/
-hash -d dl=~/Downloads
+hash -d d=~/downloads/firefox
+hash -d dt=~/downloads/torrents
+hash -d ge=/etc/portage
+hash -d 3d=~/docs/3dprint
+hash -d pm3=~/sec/dumps
